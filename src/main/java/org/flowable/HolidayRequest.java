318171c89308db78228599ad5d2d39331c6e7aec
imhansai <imhansai@foxmail.com>
2023-08-22 14:20:37 +0800

    flowable BPMN User Guide example

---
+++ src/main/java/org/flowable/HolidayRequest.java
@@ -0,0 +1,86 @@
+package org.flowable;
+
+import org.flowable.engine.*;
+import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.flowable.engine.repository.Deployment;
+import org.flowable.engine.repository.ProcessDefinition;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HolidayRequest {
+
+    public static void main(String[] args) {
+
+        // 创建流程引擎
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+                .setJdbcUrl("jdbc:h2:mem:flowable;DB_CLOSE_DELAY=-1")
+                .setJdbcUsername("sa")
+                .setJdbcPassword("")
+                .setJdbcDriver("org.h2.Driver")
+                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        ProcessEngine processEngine = cfg.buildProcessEngine();
+
+        // 部署流程定义
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("holiday-request.bpmn20.xml")
+                .deploy();
+
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deployment.getId())
+                .singleResult();
+        System.out.println("Found process definition : " + processDefinition.getName());
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Who are you?");
+        String employee = scanner.nextLine();
+
+        System.out.println("How many holidays do you want to request?");
+        Integer nrOfHolidays = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Why do you need them?");
+        String description = scanner.nextLine();
+
+
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("employee", employee);
+        variables.put("nrOfHolidays", nrOfHolidays);
+        variables.put("description", description);
+
+        // 启动流程实例
+        ProcessInstance processInstance =
+                runtimeService.startProcessInstanceByKey("holidayRequest", variables);
+
+        // 查询仅返回"managers"组的任务
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("managers").list();
+        System.out.println("You have " + tasks.size() + " tasks:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ") " + tasks.get(i).getName());
+        }
+
+        System.out.println("Which task would you like to complete?");
+        int taskIndex = Integer.parseInt(scanner.nextLine());
+        // 获取任务
+        Task task = tasks.get(taskIndex - 1);
+        Map<String, Object> processVariables = taskService.getVariables(task.getId());
+        System.out.println(processVariables.get("employee") + " wants " +
+                processVariables.get("nrOfHolidays") + " of holidays. Do you approve this?");
+
+        // 完成任务
+        boolean approved = scanner.nextLine().equalsIgnoreCase("y");
+        variables = new HashMap<>();
+        variables.put("approved", approved);
+        taskService.complete(task.getId(), variables);
+    }
+
+}
